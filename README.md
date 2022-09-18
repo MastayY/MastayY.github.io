7c2b2ba507fa53861f5b301194a4731ba553b84d
Mastay <nasywanfadilah@gmail.com>
2022-09-18 20:07:45 +0700

    Finis

---
+++ README.md
@@ -0,0 +1,11 @@
+# MastayY.github.io
+This is my first portfolio website.
+
+I'm Using :
+  1. HTML
+  2. CSS
+  3. Javascript
+  4. AOS CSS Animation
+  5. FormSubmit
+
+Progress : 85%
---
+++ js/script.js
@@ -0,0 +1,25 @@
+// responsive navbar
+const menuToggle = document.querySelector('.menu-toggle input')
+const nav = document.querySelector('.navbar ul')
+
+menuToggle.addEventListener('click', function () {
+    nav.classList.toggle('active');
+});
+
+// text typing
+const text = document.querySelector(".sec-text");
+
+        const textLoad = () => {
+            setTimeout(() => {
+                text.textContent = "Web Developer";
+            }, 0);
+            setTimeout(() => {
+                text.textContent = "Student";
+            }, 4000);
+            setTimeout(() => {
+                text.textContent = "UI/UX Designer";
+            }, 8000); 
+        }
+
+        textLoad();
+        setInterval(textLoad, 12000);
